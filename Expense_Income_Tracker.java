4cc3dbc2e1940e9eebbc05c86902d128d194d0cd
TanishaParmar2841 <149524046+TanishaParmar2841@users.noreply.github.com>
2024-08-31 23:53:26 +0530

    Add files via upload

---
+++ Expense_Income_Tracker.java
@@ -0,0 +1,18 @@
+package expenseTracker;
+
+/**
+ *
+ * @author 1BestCsharp
+ */
+public class Expense_Income_Tracker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        new ExpensesIncomesTracker().setLocationRelativeTo(null);
+
+    }
+
+}
